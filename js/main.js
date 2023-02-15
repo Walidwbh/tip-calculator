0e36f5d18a055d4a454d19469c1b3210dea89e31
walidwbh <93353066+Walidwbh@users.noreply.github.com>
2023-02-15 13:34:48 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,60 @@
+const bill = document.querySelector("input[id=bill]");
+const people = document.querySelector("input[id=poeple]");
+const tipAmount = document.getElementById("tip-amount");
+const totalAmount = document.getElementById("total-amount");
+const tips = document.querySelectorAll(".tip");
+const tipPerPerson = document.getElementById("tip-person");
+const tipTotal = document.getElementById("tip-total");
+const customTip = document.getElementById("custom");
+// console.log(tips)
+// we make the event listener
+bill.addEventListener("input",billFunction);
+people.addEventListener("input",poepleFunction);
+tips.forEach((t)=>{
+    t.addEventListener("click",tipsFunction)
+});
+customTip.addEventListener("input",customTipFunction);
+//we define the values
+let billValue = 0.0;
+let poepleValue = 0;
+let tip = 0;
+// functions
+function billFunction(){
+    billValue = parseFloat(bill.value);
+    calculateTip();
+}
+function poepleFunction(){
+    poepleValue = parseFloat(people.value);
+    if(people.value<1){
+        people.classList.add("error");
+    }else{
+        people.classList.remove("error");
+    }
+    calculateTip();
+}
+function tipsFunction(event){
+    customTip.value = "";
+    tips.forEach((t)=>{
+        t.classList.remove("selected");
+        if(event.target.innerHTML == t.innerHTML){
+            event.target.classList.add("selected");
+            tip = event.target.dataset.value / 100;
+        }
+    });
+    calculateTip();
+}
+function customTipFunction(){
+    tip = customTip.value / 100;
+    tips.forEach((t)=>{
+        t.classList.remove("selected");
+    });
+    calculateTip();
+}
+function calculateTip(){
+    if(poepleValue>=1){
+        let tipAmount = (billValue * tip)/ poepleValue;
+        let totalAmount = (billValue / poepleValue) + tipAmount;
+        tipPerPerson.innerHTML = tipAmount.toFixed(2);
+        tipTotal.innerHTML = totalAmount.toFixed(2);
+    }
+}
